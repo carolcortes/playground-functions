de9222c1f94b2e030a15015234d3f111b5eef913
carolcortes <caroline.ocortes@gmail.com>
2022-04-13 00:51:38 -0300

    arrumar a 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,16 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if (array.length > 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let number of array) {
+    if (number < 0 || number > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return `(${array.slice(0, 2).join('')}) ${array.slice(2, 7).join('')}-${array.slice(7, 12).join('')}`;
 }
-
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   let triangle = false;
